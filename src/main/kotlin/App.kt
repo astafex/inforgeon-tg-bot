38b8f524314ef79becf3fbf5c4dfcf26d3260c13
Романова Ирина Дмитриевна <IDmRomanova@sberbank.ru>
2023-02-13 19:53:46 +0300

    bot start

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,8 +1,11 @@
 package inforgeon
 
+import inforgeon.inforgeon.bot.InforgeonBot
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.telegram.telegrambots.ApiContextInitializer
+import org.telegram.telegrambots.meta.TelegramBotsApi
 
 @SpringBootApplication
 //    (exclude = [DataSourceAutoConfiguration::class])
@@ -10,5 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 class App
 
 fun main(args: Array<String>) {
+    ApiContextInitializer.init()
+    TelegramBotsApi().registerBot(InforgeonBot())
     runApplication<App>(*args)
 }
--- src/main/kotlin/inforgeon/repository/RssEntryRepository.kt
+++ src/main/kotlin/inforgeon/repository/RssEntryRepository.kt
@@ -5,16 +5,27 @@ import inforgeon.inforgeon.constant.RssTopicName
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import java.util.*
-
+import java.util.Optional
 
 interface RssEntryRepository : JpaRepository<RssEntry, Long> {
 
+    /**
+     * Поиск записей по id
+     * @param ids список идентификаторов записей
+     */
     fun findAllByIdIn(ids : List<Long>) : List<RssEntry>
 
+    /**
+     * Поиск первой записи по заданному топику (с сортировкой по убыванию)
+     * @param topic топик для фильтрации
+     */
     fun findFirstByTopicOrderByIdDesc(topic: RssTopicName) : Optional<RssEntry>
 
-    /** Не работает в jpql */
+    /**
+     * Поиск первой записи по топику и тегу без дизлайков
+     * @param topic топик для фильтрации
+     * @param tags список тегов
+     * Не работает в jpql */
     @Query(value = """
         select * from bot.rss_entry 
         where topic = :topic and id not in 
@@ -27,6 +38,11 @@ interface RssEntryRepository : JpaRepository<RssEntry, Long> {
     """, nativeQuery = true)
     fun findFirstWithoutDisliked(@Param("topic") topic: RssTopicName, @Param("tags") tags: List<String>) : Optional<RssEntry>
 
+    /**
+     * Поиск первой записи по указанному топику и id???
+     * @param topic топик для фильтрации
+     * @param id id записи
+     */
     fun findFirstByTopicAndIdLessThanOrderByIdDesc(topic: RssTopicName, id: Long) : Optional<RssEntry>
 
     /** Не работает в jpql */
@@ -44,9 +60,24 @@ interface RssEntryRepository : JpaRepository<RssEntry, Long> {
                                 @Param("id") id: Long,
                                 @Param("tags") tags: List<String>) : Optional<RssEntry>
 
+    /**
+     * Поиск записей по тегам
+     * @param tags список тегов
+     */
     fun findAllByTagsIn(tags: List<String>) :  List<RssEntry>
+
+    /**
+     * Поиск записей по топику и id???
+     * @param topic топик для фильтрации
+     * @param ids список идентификаторов записей
+     */
     fun findFirstByTopicAndIdNotInOrderByIdDesc(topic: RssTopicName, ids: List<Long>) : Optional<RssEntry>
+
+    /**
+     * Поиск записей по топику и id???
+     * @param topic топик для фильтрации
+     * @param ids список идентификаторов записей
+     * @param id id записи
+     */
     fun findFirstByTopicAndIdNotInAndIdLessThanOrderByIdDesc(topic: RssTopicName, ids: List<Long>, id: Long) : Optional<RssEntry>
-
-
 }
