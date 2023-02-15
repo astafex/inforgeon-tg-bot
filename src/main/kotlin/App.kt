ffbc2e224cf21984b7d3a0ec1a99d14c9688f61c
Artem Astafev <astafev.ph@gmail.com>
2023-02-15 22:29:39 +0300

    init bot

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,11 +1,8 @@
 package inforgeon
 
-import inforgeon.inforgeon.bot.InforgeonBot
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.telegram.telegrambots.ApiContextInitializer
-import org.telegram.telegrambots.meta.TelegramBotsApi
 
 @SpringBootApplication
 //    (exclude = [DataSourceAutoConfiguration::class])
@@ -13,7 +10,5 @@ import org.telegram.telegrambots.meta.TelegramBotsApi
 class App
 
 fun main(args: Array<String>) {
-    ApiContextInitializer.init()
-    TelegramBotsApi().registerBot(InforgeonBot())
     runApplication<App>(*args)
 }
--- src/main/kotlin/inforgeon/bot/InforgeonBot.kt
+++ src/main/kotlin/inforgeon/bot/InforgeonBot.kt
@@ -1,35 +1,104 @@
 package inforgeon.inforgeon.bot
 
-import inforgeon.inforgeon.service.UserSettingsService
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
+import inforgeon.inforgeon.service.BotApiService
+import mu.KotlinLogging
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
+import javax.annotation.PostConstruct
 
-@Service
-class InforgeonBot : TelegramLongPollingBot() {
-    @Autowired
-    private lateinit var userSettingsService: UserSettingsService
 
-    override fun getBotToken() = "6244400944:AAFKBxMZEImkHAKy4SjsrBVWtLiPg21C_zE"
+@Component
+class InforgeonBot(
+    @Value("\${inforgeon-bot.token}") private val token: String,
+    @Value("\${inforgeon-bot.name}") private val name: String,
+    private val botApiService: BotApiService
+) : TelegramLongPollingBot(token) {
+    private val log = KotlinLogging.logger {}
 
-    override fun getBotUsername() = "inforgeon_bot"
+    @PostConstruct
+    fun registerBot() {
+        TelegramBotsApi(DefaultBotSession::class.java).registerBot(this)
+        log.info { "Started bot $name, token: $token" }
+    }
+
+    override fun getBotUsername() = name
 
     override fun onUpdateReceived(update: Update) {
-        val message = update.message
-        val username = update.message.from.userName
+        update.apply {
+            if (hasMessage()) {
+                val user = botApiService.userRegistration(message.from.id)
 
-        when (message.text) {
-            "/start" -> {
-//                if (userSettingsService.get(username) == null)
-//                    userSettingsService.initializeUser(username)
 
-                execute(
-                    SendMessage().setChatId(message.chatId)
-                        .setText("Привет! Я - бот Inforgeon, помогу тебе вырезать все ненужное из твоей новостной ленты.\n" +
-                                 "Для начала работы со мной можно предварительно настроить твои любимые категории. Для этого выбери команду ...")
-                )
+
+            }
+        }
+
+        if (update.hasMessage()) {
+            val currentMessage: Message = update.message
+            val currentChatId: Long = currentMessage.chatId
+
+            val keyboard = ReplyKeyboardMarkup()
+            keyboard.keyboard = listOf(
+                KeyboardRow().apply {
+                    add(KeyboardButton("TEST1"))
+                },
+                KeyboardRow().apply {
+                    add(KeyboardButton("TEST2"))
+                }
+            )
+
+
+            when (currentMessage.text) {
+                "/start" -> {
+                    execute(
+                        SendMessage().apply {
+                            replyMarkup = keyboard
+                            chatId = currentChatId.toString()
+                            text = "user: ${currentMessage.from.userName}, id: ${currentMessage.from.id}"
+                        }
+                    )
+                }
+
+                "/some" -> {
+                    execute(
+                        SendMessage().apply {
+                            replyMarkup = InlineKeyboardMarkup().also {
+                                it.keyboard = listOf(
+                                    listOf(
+                                        InlineKeyboardButton("LOL1").apply { callbackData = "LOLLL1" },
+                                        InlineKeyboardButton("LOL2").apply { callbackData = "LOLLL2" },
+                                    )
+                                )
+                            }
+                            chatId = currentChatId.toString()
+                            text = "Choice:"
+
+                        }
+                    )
+                }
+                /*                    if (userSettingsService.get(username) == null)
+                                        userSettingsService.initializeUser(username)
+
+                                    execute(
+
+                                        SendMessage()
+                                            .setChatId(message.chatId)
+                                            .setText(
+                                                "Привет! Я - бот Inforgeon, помогу тебе вырезать все ненужное из твоей новостной ленты.\n" +
+                                                        "Для начала работы со мной можно предварительно настроить твои любимые категории. Для этого выбери команду ..."
+                                            )
+                                    )*/
             }
         }
     }
