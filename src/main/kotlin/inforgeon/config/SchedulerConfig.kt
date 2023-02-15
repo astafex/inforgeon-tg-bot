2f9a20e43d15f7fbee67d42a48c9fdedec2b8f8c
Artem Astafev <astafev.ph@gmail.com>
2023-02-15 22:27:07 +0300

    удалены лишние пробелы из конфига планировщика

--- src/main/kotlin/inforgeon/config/SchedulerConfig.kt
+++ src/main/kotlin/inforgeon/config/SchedulerConfig.kt
@@ -4,10 +4,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
 
-
 @Configuration
 @EnableScheduling
 @ConditionalOnProperty(name = ["rss.scheduler.enabled"], matchIfMissing = true)
-class SchedulerConfig {
-
-}
+class SchedulerConfig
